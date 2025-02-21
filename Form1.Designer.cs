df56044086c4ab284d4cdedf2c36dd9ebf0dc3c1
TUBABYMIA <TUBABYMIA@DESKTOP-E0TEKGF>
2025-02-20 22:05:31 -0800

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace interfazGrafica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pbImagen1 = new System.Windows.Forms.PictureBox();
+            this.pbImagen2 = new System.Windows.Forms.PictureBox();
+            this.pbImagen3 = new System.Windows.Forms.PictureBox();
+            this.pbImagen4 = new System.Windows.Forms.PictureBox();
+            this.pbImagen5 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.pbImagen1);
+            this.flowLayoutPanel1.Controls.Add(this.pbImagen2);
+            this.flowLayoutPanel1.Controls.Add(this.pbImagen3);
+            this.flowLayoutPanel1.Controls.Add(this.pbImagen4);
+            this.flowLayoutPanel1.Controls.Add(this.pbImagen5);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(4, 5);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(850, 585);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // pbImagen1
+            // 
+            this.pbImagen1.Location = new System.Drawing.Point(4, 5);
+            this.pbImagen1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbImagen1.Name = "pbImagen1";
+            this.pbImagen1.Size = new System.Drawing.Size(232, 232);
+            this.pbImagen1.TabIndex = 0;
+            this.pbImagen1.TabStop = false;
+            // 
+            // pbImagen2
+            // 
+            this.pbImagen2.Location = new System.Drawing.Point(244, 5);
+            this.pbImagen2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbImagen2.Name = "pbImagen2";
+            this.pbImagen2.Size = new System.Drawing.Size(264, 232);
+            this.pbImagen2.TabIndex = 1;
+            this.pbImagen2.TabStop = false;
+            // 
+            // pbImagen3
+            // 
+            this.pbImagen3.Location = new System.Drawing.Point(516, 5);
+            this.pbImagen3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbImagen3.Name = "pbImagen3";
+            this.pbImagen3.Size = new System.Drawing.Size(326, 232);
+            this.pbImagen3.TabIndex = 2;
+            this.pbImagen3.TabStop = false;
+            this.pbImagen3.Click += new System.EventHandler(this.pbImagen3_Click);
+            // 
+            // pbImagen4
+            // 
+            this.pbImagen4.Location = new System.Drawing.Point(4, 247);
+            this.pbImagen4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbImagen4.Name = "pbImagen4";
+            this.pbImagen4.Size = new System.Drawing.Size(225, 255);
+            this.pbImagen4.TabIndex = 3;
+            this.pbImagen4.TabStop = false;
+            // 
+            // pbImagen5
+            // 
+            this.pbImagen5.Location = new System.Drawing.Point(237, 247);
+            this.pbImagen5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbImagen5.Name = "pbImagen5";
+            this.pbImagen5.Size = new System.Drawing.Size(272, 255);
+            this.pbImagen5.TabIndex = 4;
+            this.pbImagen5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.PictureBox pbImagen1;
+        private System.Windows.Forms.PictureBox pbImagen2;
+        private System.Windows.Forms.PictureBox pbImagen3;
+        private System.Windows.Forms.PictureBox pbImagen4;
+        private System.Windows.Forms.PictureBox pbImagen5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace interfazGrafica
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            CargarImagenes();
+        }
+        private void CargarImagenes()
+        {
+            // Obtener el directorio del programa y la carpeta "imag"
+            string directorioRaiz = Application.StartupPath;
+            string rutaCarpetaImagenes = Path.Combine(directorioRaiz, "imag");
+
+            // Verificar si la carpeta existe
+            if (!Directory.Exists(rutaCarpetaImagenes))
+            {
+                MessageBox.Show("La carpeta 'imag' no existe.\nRuta: " + rutaCarpetaImagenes, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Obtener imágenes (máximo 5)
+            string[] archivos = Directory.GetFiles(rutaCarpetaImagenes, "*.*")
+                                         .Where(file => file.EndsWith(".jpg") || file.EndsWith(".jpeg") || file.EndsWith(".png") || file.EndsWith(".bmp"))
+                                         .Take(5)
+                                         .ToArray();
+
+            if (archivos.Length == 0)
+            {
+                MessageBox.Show("No se encontraron imágenes en la carpeta 'imag'.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Asignar imágenes a los PictureBox y agregar eventos Click
+            AsignarImagenAPictureBox(pbImagen1, archivos, 0);
+            AsignarImagenAPictureBox(pbImagen2, archivos, 1);
+            AsignarImagenAPictureBox(pbImagen3, archivos, 2);
+            AsignarImagenAPictureBox(pbImagen4, archivos, 3);
+            AsignarImagenAPictureBox(pbImagen5, archivos, 4);
+        }
+        private void AsignarImagenAPictureBox(PictureBox pictureBox, string[] archivos, int index)
+        {
+            if (index < archivos.Length)
+            {
+                pictureBox.Image = Image.FromFile(archivos[index]);
+                pictureBox.Tag = archivos[index]; // Guardamos la ruta de la imagen
+                pictureBox.Click += PictureBox_Click; // Evento Click
+            }
+        }
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            PictureBox clickedPictureBox = sender as PictureBox;
+
+            if (clickedPictureBox != null && clickedPictureBox.Tag != null)
+            {
+                string rutaImagen = clickedPictureBox.Tag.ToString();
+                MostrarImagenTamañoCompleto(rutaImagen);
+            }
+        }
+        private void MostrarImagenTamañoCompleto(string rutaImagen)
+        {
+            Form ventanaImagen = new Form
+            {
+                Text = Path.GetFileName(rutaImagen),
+                Width = 800,
+                Height = 600
+            };
+
+            PictureBox pictureBox = new PictureBox
+            {
+                Image = Image.FromFile(rutaImagen),
+                SizeMode = PictureBoxSizeMode.Zoom,
+                Dock = DockStyle.Fill
+            };
+
+            ventanaImagen.Controls.Add(pictureBox);
+            ventanaImagen.Show();
+        }
+
+        private void pbImagen3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
